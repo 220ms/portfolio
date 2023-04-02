27d4797cd13c024d510a847262c570e894bdc580
mhic0003 <mhic0003@student.monash.edu>
2023-04-02 14:50:57 +1000

    fixed the view projects button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,23 @@
 import '../css/style.css';
-import { FaCode, FaDownload } from 'react-icons/fa';
+import { FaCode} from 'react-icons/fa';
+
+
 
 export default function Hero() {
+
+  const viewProjects = () => {
+    document.getElementById('projects').scrollIntoView({behavior: 'smooth'});
+  };
+
   return (
     <div className="hero-container">
       <div className="hero-text">
         <h1 className="heading"><span className="blue">Aspiring</span> <span >Developer</span></h1>
         <p>Hi My name is Matt Hicks, I'm a graduating IT student from Monash University. I love to build different types of applications ranging from hobby scripts all the way through to full web applications. </p>
         <div className="cta-buttons">
-          <button className="cta-button blue-button">
+          <button className="cta-button blue-button" id="view-projects" onClick={viewProjects}>
             <FaCode className="button-icon" />
             View Projects
-          </button>
-          <button className="cta-button ghost-button">
-            <FaDownload className="button-icon" />
-            Download Resume
           </button>
         </div>
       </div>
