632e0694910c7d233a4f946584c3f9c119e84f1b
mhic0003 <mhic0003@student.monash.edu>
2023-04-21 10:37:54 +1000

    changed to IT Professional

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,7 +12,7 @@ export default function Hero() {
   return (
     <div className="hero-container">
       <div className="hero-text">
-        <h1 className="heading"><span className="blue">Aspiring</span> <span >Developer</span></h1>
+        <h1 className="heading"><span className="blue">Aspiring</span> <span >IT Professional</span></h1>
         <p>Hi My name is Matt Hicks, I'm a graduating IT student from Monash University. I love to build different types of applications ranging from hobby scripts all the way through to full web applications. </p>
         <div className="cta-buttons">
           <button className="cta-button blue-button" id="view-projects" onClick={viewProjects}>
