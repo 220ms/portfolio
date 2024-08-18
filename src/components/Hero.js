7f7b4e8f0f3f614066bf271ce4379a50508292ef
220ms <mjhicks0013@gmail.com>
2024-08-18 15:53:35 +1000

    update hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,8 +12,8 @@ export default function Hero() {
   return (
     <div className="hero-container">
       <div className="hero-text">
-        <h1 className="heading"><span className="blue">Aspiring</span> <span >IT Professional</span></h1>
-        <p>Hi My name is Matt Hicks, I'm a graduating IT student from Monash University. I love to build different types of applications ranging from hobby scripts all the way through to full web applications. </p>
+        <h1 className="heading"><span className="blue">Current</span> <span >Software Developer</span></h1>
+        <p>Hi, my name is Matt Hicks. I'm a software developer with a passion for building a wide range of applications, from hobby scripts to full-scale web applications. </p>
         <div className="cta-buttons">
           <button className="cta-button blue-button" id="view-projects" onClick={viewProjects}>
             <FaCode className="button-icon" />
