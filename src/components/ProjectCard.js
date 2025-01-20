5f22b683da39ce8b99dcec0398d492a8fe19b435
220ms <mjhicks0013@gmail.com>
2025-01-20 17:32:08 +1100

    update my website with pet social sphere project

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,58 +1,55 @@
 import React from "react";
-import {useEffect } from 'react';
+import { useEffect } from "react";
 
 export default function ProjectCard(props) {
-
   useEffect(() => {
     const card = document.getElementById(props.id);
-    const popup = document.getElementById('popup-'+ props.id);
-    const closeBtn = document.getElementById('close-' + props.id);
+    const popup = document.getElementById("popup-" + props.id);
+    const closeBtn = document.getElementById("close-" + props.id);
 
     const openCard = () => {
-      popup.style.display = 'flex';
-    }
+      popup.style.display = "flex";
+    };
 
     const closeCard = () => {
-      popup.style.display = 'none';
-    }
+      popup.style.display = "none";
+    };
 
-    card.addEventListener('click', openCard);
+    card.addEventListener("click", openCard);
 
-    closeBtn.addEventListener('click', closeCard)
+    closeBtn.addEventListener("click", closeCard);
 
     return () => {
-      card.removeEventListener('click', openCard);
-      closeBtn.removeEventListener('click', closeCard);
+      card.removeEventListener("click", openCard);
+      closeBtn.removeEventListener("click", closeCard);
     };
-  }, [props.title,props.id]);
+  }, [props.title, props.id]);
 
   return (
     <div>
       <div className="project-card" id={props.id}>
         <img src={require(`../images/${props.image}`)} alt={props.title} />
-        <div className="caption"><h1>{props.title}</h1></div>
+        <div className="caption">
+          <h1>{props.title}</h1>
+        </div>
       </div>
-      <div className="popup-card" id={"popup-"+props.id}>
+      <div className="popup-card" id={"popup-" + props.id}>
         <div className="popup-content">
           <h2>{props.title}</h2>
           <p>{props.description}</p>
           <h3>Learning Areas</h3>
-            {props.technologies.map(function(item) {
-              return (
-                <p>{item}</p>
-              );
-            })}
-          <a href={props.link} target='_blank' rel="noreferrer">Github Repo</a>
-          
-          <button className="card-close-btn" id={'close-' + props.id}>Close</button>
+          {props.technologies.map(function (item) {
+            return <p>{item}</p>;
+          })}
+          <a href={props.link} target="_blank" rel="noreferrer">
+            View
+          </a>
+
+          <button className="card-close-btn" id={"close-" + props.id}>
+            Close
+          </button>
         </div>
       </div>
     </div>
   );
 }
-
-
-
-
-
-
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,35 +1,41 @@
 const Projects = [
-    {
-        image: 'compare.png',
-        title: 'Legal Hackathon',
-        description: `My team for the Monash Legal Hackathon created an AI-based application to solve a legal sector problem. 
+  {
+    image: "petsocialsphere.png",
+    title: "PetSocialSphere",
+    description: `PetSocialSphere is a pet-focused social media app where users can create profiles for themselves and their pets. It includes features to post, like, and comment on posts, manage friendships, and view pet profiles. The app uses JWT for authentication, with a 15-minute lifespan for the access token, and a refresh token for refreshing the access token.`,
+    technologies: [
+      "React",
+      "Node.js",
+      "JWT",
+      "Axios",
+      "TanStack Query",
+      "MongoDB",
+    ],
+    link: "https://app.petsocialsphereapi.com/",
+    id: "PetSocialSphere1",
+  },
+  {
+    image: "compare.png",
+    title: "Legal Hackathon",
+    description: `My team for the Monash Legal Hackathon created an AI-based application to solve a legal sector problem. 
         we used Python libraries, including opencv, to compare images and text to the Australian national trademark registry.
          The data was passed through a neural network to produce a score for copyright infringement. 
          I focused on UI design using the pygame module and in the end, we won the hackathon.`,
-        technologies: ['Python','AI','Pygame'],
-        link: 'https://github.com/220ms/Hackathon',
-        id: 'Card1'
-    },
-    {
-        image: 'sorted-files.png',
-        title: 'Classify Images',
-        description: `I created this code to solve a problem my parents had when they came back from their holiday in New Zealand.
+    technologies: ["Python", "AI", "Pygame"],
+    link: "https://github.com/220ms/Hackathon",
+    id: "Card1",
+  },
+  {
+    image: "sorted-files.png",
+    title: "Classify Images",
+    description: `I created this code to solve a problem my parents had when they came back from their holiday in New Zealand.
          They wanted to upload to the cloud but they had too many images in the same folder and the folder size exceeded the upload 
          capacity for OneDrive. So I decided to come up with a solution that classifies images based on the date metadata contained 
          within the NEF image files and puts them into date-based directories.`,
-        technologies: ['Python','Metadata'],
-        link: 'https://github.com/220ms/sort-image-by-date',
-        id: 'Card2'
-    },
-    {
-        image: 'ecommerce.png',
-        title: 'E-Commerce site',
-        description: `I developed an E-Commerce website using CakePHP for my final year project. We created a product management system for easy purchasing and added a secure payment gateway through PayPal.
-         Despite the challenges, my team delivered a functional site and gained valuable experience in web development and project management.`,
-        technologies: ['CakePHP','Web development','SQL','Project Management'],
-        link: 'https://github.com/220ms/JJCrackers-Lavosh',
-        id: 'Card3'
-    }
-]
+    technologies: ["Python", "Metadata"],
+    link: "https://github.com/220ms/sort-image-by-date",
+    id: "Card2",
+  },
+];
 
 export default Projects;
